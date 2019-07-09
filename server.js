cad47d22d780b5fed90d499bf6f092d385a0c88a
douglasfreire <douglasfreire20@gmail.com>
2019-07-08 22:03:20 -0300

    Reestruturação dos arquivos

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+//Iniciando o app
+const app = express();
+
+//Iniciando o mongodb
+mongoose.connect("mongodb://localhost:27017/nodeapi", {useNewUrlParser: true});
+
+//Biblioteca require-dir instalada para facilitar a integração do model
+requireDir('./src/model/');
+
+//Rotas
+app.use('/', require('./src/routes'));
+
+app.listen(3000, function() {
+    console.log("Servidor rodando na porta 3000!");
+    console.log("MongoDB rondando na porta 27017!");
+});
