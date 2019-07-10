abc02f42452730f6bc1835858ada8f0407d60624
douglasfreire <douglasfreire20@gmail.com>
2019-07-10 20:45:49 -0300

    adição do CRUD completo

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,14 @@ const routes = express.Router();
 
 const ProductController = require('../src/controller/ProductController')
 
+routes.get("/", (req, res) => {
+    res.send("Backend Ativo");
+});
+
 routes.get("/products", ProductController.index);
-routes.post('/products', ProductController.store);
+routes.post("/products", ProductController.store);
+routes.get("/products/:id", ProductController.show);
+routes.put("/products/:id", ProductController.update);
+routes.delete("/products/:id", ProductController.destroy);
 
 module.exports = routes;
